649206c27c757269f8671b82964e7f9a06878950
attdona <attilio.dona@gmail.com>
2026-09-02 12:35:31 +0200

    bump version 0.8.15 -> 0.8.16

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Rembus"
 copyright = "2026, Attilio Donà"
 author = "Attilio Donà"
-release = "0.8.15"
+release = "0.8.16"
 html_baseurl = "https://cardo-org.github.io/rembus.python/"
 
 # -- General configuration ---------------------------------------------------
